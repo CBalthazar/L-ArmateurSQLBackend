78294602f9d236a5efea1648b00244b07153725b
Fatah872218 <fatah_djeddi@yahoo.fr>
2025-02-04 15:27:29 +0100

    creation de server   router et controllers

---
+++ controllers/controllerArtist.js
@@ -0,0 +1,17 @@
+const express = require("express");
+class artisteController {
+  creatArtiste(req, res) {
+    res.send("post artiste ajouté");
+  }
+  readArtiste(req, res) {
+    res.send("messgae Artiste");
+  }
+  updateArtiste(req, res) {
+    res.send("artiste mis a jour");
+  }
+  deleteArtiste(req, res) {
+    res.send("artiste supprimé");
+  }
+}
+
+module.exports = new artisteController();
