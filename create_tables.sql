ffd523e4f4656e0371b993bb686cdfdaf98950fa
seliegarrigues <eliseg64@gmail.com>
2025-02-04 15:08:17 +0100

    creation tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,38 +1,54 @@
-CREATE DATABASE IF NOT EXISTS L-ArmateurSQLBackend
-USE L-ArmateurSQLBackend
+CREATE DATABASE IF NOT EXISTS ArmateurSQLBackend;
+USE ArmateurSQLBackend;
 
 DROP TABLE IF EXISTS oeuvres_mouvements;
 DROP TABLE IF EXISTS mouvements_artistes;
-DROP TABLE IF EXISTS mouvements;
-DROP TABLE IF EXISTS artists;
 DROP TABLE IF EXISTS oeuvres;
+DROP TABLE IF EXISTS mouvements;
+DROP TABLE IF EXISTS artistes;
 
-CREATE TABLES artistes (
-    idArtiste INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE artistes (
+    idArtiste INT PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
-    contenu VARCHAR (1000) NOT NULL,
+    contenu VARCHAR(1000) NOT NULL,
     date_debut DATE NOT NULL,
-    date_fin DEFAULT NULL,
+    date_fin DATE DEFAULT NULL,
     image_url VARCHAR(255)
 );
-CREATE TABLES mouvements (
-    idMouvement INT AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE mouvements (
+    idMouvement INT PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
-    contenu VARCHAR (150) NOT NULL,
+    contenu VARCHAR(150) NOT NULL,
     date_debut DATE NOT NULL,
-    date_fin DEFAULT NULL,
-     image_url VARCHAR(255)
-    
+    date_fin DATE DEFAULT NULL,
+    image_url VARCHAR(255)
 );
 
-CREATE TABLES oeuvres (
-    idOeuvre INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE oeuvres (
+    idOeuvre INT PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
-    contenu VARCHAR (1000) NOT NULL,
+    contenu VARCHAR(1000) NOT NULL,
     date_debut DATE NOT NULL,
-    date_fin DEFAULT NULL,
+    date_fin DATE DEFAULT NULL,
     image_url VARCHAR(255),
     idArtiste INT,
-    stock INT CHECK (stock >= 0)
+    stock INT CHECK (stock >= 0),
     FOREIGN KEY (idArtiste) REFERENCES artistes(idArtiste) ON DELETE SET NULL
+);
+
+CREATE TABLE oeuvres_mouvements (
+    idOeuvre INT,
+    idMouvement INT,
+    PRIMARY KEY (idOeuvre, idMouvement),
+    FOREIGN KEY (idOeuvre) REFERENCES oeuvres(idOeuvre) ON DELETE CASCADE,
+    FOREIGN KEY (idMouvement) REFERENCES mouvements(idMouvement) ON DELETE CASCADE
+);
+
+CREATE TABLE mouvements_artistes (
+    idMouvement INT,
+    idArtiste INT,
+    PRIMARY KEY (idMouvement, idArtiste),
+    FOREIGN KEY (idMouvement) REFERENCES mouvements(idMouvement) ON DELETE CASCADE,
+    FOREIGN KEY (idArtiste) REFERENCES artistes(idArtiste) ON DELETE CASCADE
 );
